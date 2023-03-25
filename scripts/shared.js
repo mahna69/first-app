6333e40ab72fd9eb7b3e8043f874369105752806
mahna <mahnamohammadi124@gmail.com>
2023-03-25 22:01:54 +0430

    add meta tag

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,10 +1,19 @@
 let backdrop = document.querySelector('.backdrop')
 let modal = document.querySelector('.modal')
 let selectPlanBtn = document.querySelectorAll('.plan button')
+let modalCloseBtn = document.querySelector('.modal__action--negative')
 
 for(let i=0;i < selectPlanBtn.length;i++){
     selectPlanBtn[i].addEventListener('click',() => {
 modal.style.display='block'
-ba.style.display='block'
+backdrop.style.display='block'
     })
+}
+
+backdrop.addEventListener('click',modalClose)
+modalCloseBtn.addEventListener('click',modalClose)
+
+function modalClose() {
+    backdrop.style.display='none'
+    modal.style.display='none'
 }
